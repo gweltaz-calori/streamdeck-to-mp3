cfa213a19ccea43cf63b2cb093295c1010083be2
Gweltaz <gwel.74@gmail.com>
2021-08-04 12:23:14 +0200

    more

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const clipboardy = require("clipboardy");
 const { exec, spawn } = require("child_process");
 const path = require("path");
 const fs = require("fs");
+var https = require("follow-redirects").https;
+const VERSION_STATUS = require("./enums");
 
 let mainWindow;
 
@@ -17,60 +19,107 @@ function createWindow() {
       nodeIntegration: true,
       contextIsolation: false,
     },
-  });.
+  });
   mainWindow.loadFile("index.html");
-
 }
 
 app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", function () {
-   
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
-  mainWindow.webContents.on("did-finish-load", () => {
-    let ytbPath = path.join(__dirname, "youtube-dl", "youtube-dl.exe");
+  let ytbPath = path.join(__dirname, "youtube-dl", "youtube-dl.exe");
 
-    if (__dirname.includes("app.asar")) {
-      ytbPath = path.join(
-        __dirname.replace("app.asar", "app.asar.unpacked"),
-        "youtube-dl",
-        "youtube-dl.exe"
-      );
-    }
+  if (__dirname.includes("app.asar")) {
+    ytbPath = path.join(
+      __dirname.replace("app.asar", "app.asar.unpacked"),
+      "youtube-dl",
+      "youtube-dl.exe"
+    );
+  }
 
-    ipcMain.on("clipboard", (e, value) => {
-      const ytb = spawn(ytbPath, [
-        "--extract-audio",
-        "--audio-format",
-        "mp3",
-        value,
-        "-o",
-        `${process.env.USERPROFILE}\\Downloads\\%(title)s.%(ext)s`,
-      ]);
-      ytb.stdout.on("data", (data) => {
-        let stringifyData = data.toString();
-        if (stringifyData.includes("%")) {
-          const regex = /([0-9.]+)%/;
-          const progress = regex.exec(stringifyData)[1];
-          mainWindow.webContents.send("progress", progress);
+  mainWindow.webContents.send("checking-version");
+  https.get("https://rg3.github.io/youtube-dl/update/versions.json", (res) => {
+    let rawData = "";
+    res.on("data", (chunk) => {
+      rawData += chunk;
+    });
+    res.on("end", (_) => {
+      let lastVersion = JSON.parse(rawData)["latest"].trim();
+      exec(`${ytbPath} --version`, (err, stdout) => {
+        stdout = stdout.trim();
+
+        if (lastVersion !== stdout) {
+          mainWindow.webContents.send(
+            "version-status",
+            VERSION_STATUS.NEED_UPDATE
+          );
+          const file = fs.createWriteStream(ytbPath);
+          https.get(`https://yt-dl.org/latest/youtube-dl.exe`, (response) => {
+            let total = parseInt(response.headers["content-length"]);
+            let received = 0;
+
+            response
+              .on("data", (chunk) => {
+                received += chunk.length;
+                mainWindow.webContents.send(
+                  "version-progress",
+                  ((received * 100) / total).toFixed(2)
+                );
+              })
+              .pipe(file)
+              .on("close", () => {
+                mainWindow.webContents.send("ytb-updated");
+                download();
+              });
+          });
+        } else {
+          mainWindow.webContents.send(
+            "version-status",
+            VERSION_STATUS.UP_TO_DATE
+          );
+          download();
         }
-      });
-      ytb.stderr.on("data", (data) => {});
-      ytb.on("error", (error) => {
-        app.quit();
-      });
-
-      ytb.on("close", (code) => {
-        app.quit();
       });
     });
   });
+
+  let clipboard = null;
+
+  ipcMain.on("clipboard", (e, value) => {
+    clipboard = value;
+  });
+
+  function download() {
+    const ytb = spawn(ytbPath, [
+      "--extract-audio",
+      "--audio-format",
+      "mp3",
+      clipboard,
+      "-o",
+      `${process.env.USERPROFILE}\\Downloads\\%(title)s.%(ext)s`,
+    ]);
+    ytb.stdout.on("data", (data) => {
+      let stringifyData = data.toString();
+      if (stringifyData.includes("%")) {
+        const regex = /([0-9.]+)%/;
+        const progress = regex.exec(stringifyData)[1];
+        mainWindow.webContents.send("progress", progress);
+      }
+    });
+    ytb.stderr.on("data", (data) => {});
+    ytb.on("error", (error) => {
+      app.quit();
+    });
+
+    ytb.on("close", (code) => {
+      app.quit();
+    });
+  }
 });
 
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
-
--- renderer.js
+++ renderer.js
@@ -1,7 +1,13 @@
 const { ipcRenderer } = require("electron");
+const VERSION_STATUS = require("./enums");
 
 let $progress = document.querySelector(".progress-text");
 let $progressInner = document.querySelector(".progress-inner");
+let $group = document.querySelector(".group");
+let $groupYtb = document.querySelector(".group-ytb");
+let $groupVersion = document.querySelector(".group-version");
+let $progressVersion = document.querySelector(".progress-text-version");
+let $progressInnerVersion = document.querySelector(".progress-inner-version");
 
 navigator.clipboard.readText().then((v) => {
   ipcRenderer.send("clipboard", v);
@@ -14,4 +20,24 @@ ipcRenderer.on("progress", (evt, value) => {
 
 ipcRenderer.on("ev", (e, message) => {
   console.log(message);
+});
+
+ipcRenderer.on("version-status", (e, status) => {
+  if (status == VERSION_STATUS.UP_TO_DATE) {
+    $groupVersion.style.display = "none";
+    $group.style.display = "flex";
+  } else {
+    $groupYtb.style.display = "flex";
+    $groupVersion.style.display = "none";
+  }
+});
+
+ipcRenderer.on("version-progress", (e, value) => {
+  $progressVersion.textContent = value + "%";
+  $progressInnerVersion.style.width = value + "%";
+});
+
+ipcRenderer.on("ytb-updated", () => {
+  $group.style.display = "flex";
+  $groupYtb.style.display = "none";
 });
